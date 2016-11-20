970aa481f9e4e7b9fd6aceb237b712952ba3c6be
theapache64-bravo <theapache64@gmail.com>
2016-11-20 09:35:45 +0530

    Ready to deploy - android

--- client/app/src/main/java/com/theah64/ets/utils/App.java
+++ client/app/src/main/java/com/theah64/ets/utils/App.java
@@ -36,7 +36,7 @@ import java.io.IOException;
 )
 public class App extends Application {
 
-    public static final boolean IS_DEBUG_MODE = true;
+    public static final boolean IS_DEBUG_MODE = false;
     private static final String X = App.class.getSimpleName();
 
     private static String companyCode;
