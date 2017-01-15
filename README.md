e1e17d3fcc5f665af894547ce551d9727b401579
theapache64-hotel <theapache64@gmail.com>
2017-01-15 18:44:43 +0530

    .gitignore is now working

--- README.md
+++
@@ -1,2 +0,0 @@
-# ets
-Employee Tracking System is an android project developed to track android devices (under employee-company strategy)
--- api/src/com/theah64/ets/api/models/Company.java
+++
@@ -1,8 +0,0 @@
-package com.theah64.ets.api.models;
-
-/**
- * Created by theapache64 on 18/11/16.
- */
-public class Company {
-    public static String KEY_COMPANY_CODE = "company_code";
-}
